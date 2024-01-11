b660656fd80b197fc1f14f717be6b79be7b622d2
HakanB236 <hbaysal236@gmail.com>
2024-01-11 16:42:32 +0300

    i learned sqlite crud functions

--- SqliteUsage/app/src/main/java/com/hakanbaysal20/sqliteusage/MainActivity.kt
+++ SqliteUsage/app/src/main/java/com/hakanbaysal20/sqliteusage/MainActivity.kt
@@ -2,12 +2,27 @@ package com.hakanbaysal20.sqliteusage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val hakan = Person(0,"Hakan","0541",23,1.90)
-        Persondao().addPerson(DatabaseAccess(applicationContext),hakan)
+        val hakan = Person(3,"Hakan","0101",23,1.90)
+      //  Persondao().addPerson(DatabaseAccess(applicationContext),hakan)
+        val dba = DatabaseAccess(applicationContext)
+     //   Persondao().updatePerson(dba,hakan)
+    //    Persondao().deletePerson(dba,1)
+        val personList = Persondao().getContacts(dba)
+      //  val personSearchList = Persondao().searchContact(dba,"Farukiyt")
+        val personRandomList = Persondao().getRandom(dba)
+        for (k in personRandomList){
+            Log.e("ad",k.person_name)
+            Log.e("numara",k.person_number)
+            Log.e("yaş",k.person_age.toString())
+            Log.e("boy",k.person_height.toString())
+            Log.e("No",k.person_no.toString())
+        }
+
     }
 }
--- SqliteUsage/app/src/main/java/com/hakanbaysal20/sqliteusage/Persondao.kt
+++ SqliteUsage/app/src/main/java/com/hakanbaysal20/sqliteusage/Persondao.kt
@@ -14,11 +14,87 @@ class Persondao {
         db.insertOrThrow("person",null,values)
         db.close()
     }
-    fun deletePerson() {
-        
+    fun getContacts(vba: DatabaseAccess): ArrayList<Person> {
+        val contactArrayList = ArrayList<Person>()
+        val db = vba.writableDatabase
+        val cursor = db.rawQuery("SELECT * FROM person", null)
+        val personNoIndex = cursor.getColumnIndex("person_no")
+        val personNameIndex = cursor.getColumnIndex("person_name")
+        val personNumberIndex = cursor.getColumnIndex("person_number")
+        val personAgeIndex = cursor.getColumnIndex("person_age")
+        val personHeightIndex = cursor.getColumnIndex("person_height")
+        while (cursor.moveToNext()) {
+            val person = Person(
+                cursor.getInt(personNoIndex),
+                cursor.getString(personNameIndex),
+                cursor.getString(personNumberIndex),
+                cursor.getInt(personAgeIndex),
+                cursor.getDouble(personHeightIndex)
+            )
+            contactArrayList.add(person)
+        }
+        cursor.close()
+        return contactArrayList
     }
-    fun updatePerson() {
 
+    fun updatePerson(db: DatabaseAccess,person: Person) {
+        val dba = db.writableDatabase
+        val values = ContentValues()
+        values.put("person_name",person.person_name)
+        values.put("person_number",person.person_number)
+        values.put("person_age",person.person_age)
+        values.put("person_height",person.person_height)
+        dba.update("person",values,"person_no=?", arrayOf(person.person_no.toString()))
+        dba.close()
+    }
+    fun deletePerson(vb:DatabaseAccess,person_no:Int) {
+        val dba = vb.writableDatabase
+        dba.delete("person","person_no=?", arrayOf(person_no.toString()))
+        dba.close()
+    }
+    fun searchContact(db: DatabaseAccess,keyword:String) : ArrayList<Person> {
+        val contactArrayList = ArrayList<Person>()
+        val dba = db.writableDatabase
+        val cursor = dba.rawQuery("SELECT * FROM person WHERE person_name like '%$keyword%'",null)
+        val personNoIndex = cursor.getColumnIndex("person_no")
+        val personNameIndex = cursor.getColumnIndex("person_name")
+        val personNumberIndex = cursor.getColumnIndex("person_number")
+        val personAgeIndex = cursor.getColumnIndex("person_age")
+        val personHeightIndex = cursor.getColumnIndex("person_height")
+        while (cursor.moveToNext()){
+            val person = Person(
+                cursor.getInt(personNoIndex),
+                cursor.getString(personNameIndex),
+                cursor.getString(personNumberIndex),
+                cursor.getInt(personAgeIndex),
+                cursor.getDouble(personHeightIndex)
+            )
+            contactArrayList.add(person)
+        }
+        cursor.close()
+        return contactArrayList
+    }
+    fun getRandom(db: DatabaseAccess) : ArrayList<Person> { // RASTGELE2 VERİ
+        val contactArrayList = ArrayList<Person>()
+        val dba = db.writableDatabase
+        val cursor = dba.rawQuery("SELECT * FROM person ORDER BY RANDOM() LIMIT 2",null)
+        val personNoIndex = cursor.getColumnIndex("person_no")
+        val personNameIndex = cursor.getColumnIndex("person_name")
+        val personNumberIndex = cursor.getColumnIndex("person_number")
+        val personAgeIndex = cursor.getColumnIndex("person_age")
+        val personHeightIndex = cursor.getColumnIndex("person_height")
+        while (cursor.moveToNext()){
+            val person = Person(
+                cursor.getInt(personNoIndex),
+                cursor.getString(personNameIndex),
+                cursor.getString(personNumberIndex),
+                cursor.getInt(personAgeIndex),
+                cursor.getDouble(personHeightIndex)
+            )
+            contactArrayList.add(person)
+        }
+        cursor.close()
+        return contactArrayList
     }
 
 }
