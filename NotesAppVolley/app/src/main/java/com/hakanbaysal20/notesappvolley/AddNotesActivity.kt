ea2c58052aec85de6e9c0f1e98c0de9a4e6ea220
HakanB236 <hbaysal236@gmail.com>
2024-02-18 02:21:42 +0300

    notesapp volley is completed

--- NotesAppVolley/app/src/main/java/com/hakanbaysal20/notesappvolley/AddNotesActivity.kt
+++ NotesAppVolley/app/src/main/java/com/hakanbaysal20/notesappvolley/AddNotesActivity.kt
@@ -1,15 +1,52 @@
 package com.hakanbaysal20.notesappvolley
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.hakanbaysal20.notesappvolley.databinding.ActivityAddNotesBinding
 import com.hakanbaysal20.notesappvolley.databinding.ActivityMainBinding
+import org.json.JSONException
 
 class AddNotesActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding:ActivityAddNotesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.addLessonButton.setOnClickListener {
+            AddNotes()
+        }
+    }
+    fun AddNotes() {
+        val baseUrl = "http://kasimadalan.pe.hu/notlar/insert_not.php"
+        val request = object : StringRequest(Request.Method.POST,baseUrl,Response.Listener { response ->
+            try {
+                startActivity(Intent(this@AddNotesActivity,MainActivity::class.java))
+                finish()
+                Toast.makeText(this,"Ders başarıyla eklendi",Toast.LENGTH_SHORT).show()
 
+            }catch (e:JSONException){
+                e.printStackTrace()
+            }
+        },Response.ErrorListener {error ->
+        error.printStackTrace()
+        }){
+            override fun getParams(): MutableMap<String, String>? {
+                val lessonName = binding.addLessonName.text
+                val midterm = binding.addMidtermText.text
+                val final = binding.addFinalText.text
+                val params = HashMap<String,String>()
+                params["ders_adi"] = lessonName.toString()
+                params["not1"] = midterm.toString()
+                params["not2"] = final.toString()
+                return params
+            }
+        }
+        Volley.newRequestQueue(this).add(request)
     }
 }
--- NotesAppVolley/app/src/main/java/com/hakanbaysal20/notesappvolley/MainActivity.kt
+++ NotesAppVolley/app/src/main/java/com/hakanbaysal20/notesappvolley/MainActivity.kt
@@ -16,16 +16,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var noteList:ArrayList<Note>
     private lateinit var adapter:CardViewAdapter
+    private var gpa = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         //rv
         binding.rv.setHasFixedSize(true)
         binding.rv.layoutManager = LinearLayoutManager(this@MainActivity)
         getNotes()
-
+        binding.toolbar.setSubtitle(gpa.toString())
+        setSupportActionBar(binding.toolbar)
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity,AddNotesActivity::class.java)
             startActivity(intent)
@@ -36,15 +37,20 @@ class MainActivity : AppCompatActivity() {
         noteList = ArrayList<Note>()
         val baseUrl = "http://kasimadalan.pe.hu/notlar/tum_notlar.php"
         val request = StringRequest(Request.Method.GET,baseUrl,Response.Listener{response ->
-
             try {
                 val jsonObj = JSONObject(response)
                 val notes = jsonObj.getJSONArray("notlar")
+                var total = 0
                 for (i in 0 until notes.length()){
                     val responseNote = notes.getJSONObject(i)
                     val note = Note(responseNote.getInt("not_id"),responseNote.getString("ders_adi"),responseNote.getInt("not1"),responseNote.getInt("not2"))
                     noteList.add(note)
+                    val note1 = responseNote.getString("not1").toInt()
+                    val note2 = responseNote.getString("not2").toInt()
+                    val average = (note1 + note2) / 2
+                    total += average
                 }
+                gpa = total / noteList.size
                 adapter = CardViewAdapter(this,noteList)
                 binding.rv.adapter = adapter
             }catch (e:JSONException){
@@ -56,4 +62,5 @@ class MainActivity : AppCompatActivity() {
         })
         Volley.newRequestQueue(this).add(request)
     }
+
 }
